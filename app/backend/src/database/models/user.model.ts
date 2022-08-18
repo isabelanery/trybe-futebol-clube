6a628e0ad78ea457317e99896fd4b777462dbdf3
isabelanery <isabelanery242@gmail.com>
2022-08-18 15:43:51 -0300

    feat: change dockerfile

--- app/backend/src/database/models/user.model.ts
+++ app/backend/src/database/models/user.model.ts
@@ -3,34 +3,29 @@ import db from '.';
 
 class UserModel extends Model {
   id!: number;
-  username: string;
-  email: string;
-  password: string;
-  role: string;
+  username!: string;
+  email!: string;
+  password!: string;
+  role!: string;
 }
 
 UserModel.init({
   id: {
     type: INTEGER,
-    allowNull: false,
     primaryKey: true,
     autoIncrement: true,
   },
   username: {
     type: STRING,
-    allowNull: false,
   },
   email: {
     type: STRING,
-    allowNull: false,
   },
   password: {
     type: STRING,
-    allowNull: false,
   },
   role: {
     type: STRING,
-    allowNull: false,
   },
 }, {
   sequelize: db,
