1fc04dc549afc8816fa310affd7813b33aec4705
isabelanery <isabelanery242@gmail.com>
2022-08-17 23:07:34 -0300

    feat: start tests on POST /login

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,43 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import UserModel from '../database/models/user.model';
+const UserMock = require('./mock/models/Users.json');
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('/login', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(UserModel, "findOne")
+      .resolves(UserMock.findOne as unknown as UserModel);
+  });
+
+  after(()=>{
+    (UserModel.findOne as sinon.SinonStub).restore();
+  })
+
+  it('A requisição POST para rota inicial retorna um objeto com a chave "token"', async () => {
+    chaiHttpResponse = await chai.request(app)
+      .post('/login')
+      .send({
+        email: 'user@user.com',
+        password: 'secret_user', 
+      });
+
+    expect(chaiHttpResponse).to.have.property('token');
+  });
+
+  it('Essa requisição deve retornar código de status 200', () => {
+    expect(chaiHttpResponse).to.have.status(200);
+  });
+});
