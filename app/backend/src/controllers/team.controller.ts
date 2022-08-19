2cb14a31191ef51e947d89a659508f97809131bb
isabelanery <isabelanery242@gmail.com>
2022-08-19 01:20:27 -0300

    feat: teams seeder

--- app/backend/src/controllers/team.controller.ts
+++ app/backend/src/controllers/team.controller.ts
@@ -3,7 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 import TeamService from '../services/team.service';
 
 export default class TeamController {
-  static async list(req: Request, res: Response) {
+  static async list(_req: Request, res: Response) {
     const teams = await TeamService.list();
 
     res.status(StatusCodes.OK).json(teams);
